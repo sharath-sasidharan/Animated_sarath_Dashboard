08289aba6e6d47f1cc788058dc156f5addf6045e
Sarath Sasidharan <sasidharansteven@gmail.com>
2021-12-05 22:24:41 +0530

    Added changes to Dark theme

--- js/index.js
+++ js/index.js
@@ -2,3 +2,16 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// dark theme
+
+let themeBtn = document.querySelector("#theme-btn");
+themeBtn.addEventListener("click", () => {
+  themeBtn.classList.toggle("fa-sun");
+
+  if (themeBtn.classList.contains("fa-sun")) {
+    document.body.classList.add("active");
+  } else {
+    document.body.classList.remove("active");
+  }
+});
