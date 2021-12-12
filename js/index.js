1f283b2f22c2b77ddf4bcda0e1dc3f743e5de9cb
Sarath Sasidharan <sasidharansteven@gmail.com>
2021-12-12 16:20:33 +0530

    Added Light/Dark Mode

--- js/index.js
+++ js/index.js
@@ -5,13 +5,29 @@ window.addEventListener("scroll", () => {
 
 // dark theme
 
-let themeBtn = document.querySelector("#theme-btn");
-themeBtn.addEventListener("click", () => {
-  themeBtn.classList.toggle("fa-sun");
+// let themeBtn = document.querySelector("#theme-btn");
+// themeBtn.addEventListener("click", () => {
+//   themeBtn.classList.toggle("fa-sun");
 
-  if (themeBtn.classList.contains("fa-sun")) {
-    document.body.classList.add("active");
-  } else {
-    document.body.classList.remove("active");
-  }
+//   if (themeBtn.classList.contains("fa-sun")) {
+//     document.body.classList.add("active");
+//   } else {
+//     document.body.classList.remove("active");
+//   }
+// });
+
+// Hamburger-Menu
+
+const toggle = document.querySelector(".toggle");
+const navigation = document.querySelector(".navigation");
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+// dark mode -2
+
+const checkbox = document.querySelector(".checkbox");
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
 });
